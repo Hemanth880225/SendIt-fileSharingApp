02da12efd1fd90648d8be25278e78db2bc0c821e
Hemanth <hemanth880225@gmail.com>
2026-04-21 21:05:51 +0530

    Final fix - DB init in run.py

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from sendit import app
+# from sendit import app
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from sendit import app, db
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
